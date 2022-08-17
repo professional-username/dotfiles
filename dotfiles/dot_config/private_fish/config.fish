4c0a6417a802865eed82698c5a980be9cf9dadb3
username <emailaddress@emailprovider.com>
2022-08-17 18:43:07 +0100

    fish: Add catppuccin colors to fzf

--- dotfiles/dot_config/private_fish/config.fish
+++ dotfiles/dot_config/private_fish/config.fish
@@ -1,8 +1,13 @@
 # Disable fish greeting
 set fish_greeting
 
+# Set some fish cars
 set -gx EDITOR nvim
 set -gx XDG_CONFIG_HOME $HOME/.config/
+set -gx FZF_DEFAULT_OPTS "\
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
 # Set $PATH
 fish_add_path -p $HOME/bin $HOME/.cargo/bin
