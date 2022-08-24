0a2969141e7ad908faa879a9be6f9d4c0ec6982e
username <9hacahn5@anonaddy.me>
2022-08-24 22:33:33 +0100

    exa: Add catppuccin ls_colors

--- dotfiles/dot_config/private_fish/config.fish
+++ dotfiles/dot_config/private_fish/config.fish
@@ -8,6 +8,7 @@ zoxide init fish | source
 set -gx EDITOR nvim
 set -gx XDG_CONFIG_HOME $HOME/.config/
 set -gx FZF_DEFAULT_OPTS "\
+set -gx LS_COLORS "$(cat $HOME/.config/exa/lscolors_catppuccin)"
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
