290d86de2661ade54b4cc281a16982885f56a2fc
username <9hacahn5@anonaddy.me>
2022-08-28 20:44:12 +0100

    firefox: Add some personal preferences

---
+++ dotfiles/.chezmoitemplates/Firefox/profiles/scripts/02-personal-preferences.js
@@ -0,0 +1,4 @@
+// Enable user stylesheets
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+// Set website default colorscheme to dark
+user_pref("layout.css.prefers-color-scheme.content-override", 0);
