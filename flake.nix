5122c23ea0a154c833a894a91e1addbaaa4cf7c5
professional-username <9hacahn5@anonaddy.me>
2024-05-30 14:34:46 +0100

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Your new nix config";
+
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+  in {
+    # NixOS configuration entrypoint
+    nixosConfigurations = {
+      # Config for gpteapot system
+      gpteapot = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          # System-level configuration
+	  ./nixos/configuration.nix
+
+	  home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = inputs;
+	    # User-specific configs
+            home-manager.users.username = import ./home;
+          }
+	];
+      };
+
+    };
+  };
+}
