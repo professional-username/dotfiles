dbf80a047ba4602bd995ef4c35ab45b50d4911d8
professional-username <9hacahn5@anonaddy.me>
2024-06-09 16:55:58 +0100

    Fix hyprland workspace moving

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -51,7 +51,7 @@
             ws = let c = (x + 1) / 10; in builtins.toString (x + 1 - (c * 10));
           in [
             "$mod, ${ws}, workspace, ${toString (x + 1)}"
-            "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)} silent"
+            "$mod SHIFT, ${ws}, movetoworkspacesilent, ${toString (x + 1)}"
           ]) 10));
     };
   };
