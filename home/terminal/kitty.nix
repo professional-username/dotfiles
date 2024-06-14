d6a832a041e66e62fab3cf14d626bafbf6aab947
professional-username <9hacahn5@anonaddy.me>
2024-06-14 19:33:17 +0100

    Make kitty prettier

--- home/terminal/kitty.nix
+++ home/terminal/kitty.nix
@@ -7,6 +7,13 @@
     # Environment vars
     environment = { };
 
+    extraConfig = ''
+      window_border_width 5
+      window_margin_width 10
+      window_padding_width 20
+      background_opacity 0.85
+    '';
+
     # Font
     # font = { };
   };
