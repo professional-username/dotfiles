f23c47af3d84283173d3dfb266040305d03e4f73
professional-username <9hacahn5@anonaddy.me>
2024-06-17 12:22:11 +0100

    Comment out some code that causes errors

--- system/software/nvim/completion.nix
+++ system/software/nvim/completion.nix
@@ -2,17 +2,18 @@
   plugins = {
     nvim-cmp = {
       enable = true;
-      mapping = {
-        "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-        "<C-f>" = "cmp.mapping.scroll_docs(4)";
-        "<C-e>" = "cmp.mapping.close()";
-        "<C-space>" = "cmp.mapping.complete()";
-        "<C-y>" = {
-          action = ''
-            cmp.mapping.confirm({
-                                            select = true, behavior = cmp.ConfirmBehavior.Insert })'';
-        };
-      };
+      # mapping = {
+      #   "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+      #   "<C-f>" = "cmp.mapping.scroll_docs(4)";
+      #   "<C-e>" = "cmp.mapping.close()";
+      #   "<C-space>" = "cmp.mapping.complete()";
+      #   "<C-y>" = {
+      #     action = ''
+      #       cmp.mapping.confirm({
+      #           select = true, behavior = cmp.ConfirmBehavior.Insert })
+      #     '';
+      #   };
+      # };
       window.documentation.border = [ "╭" "─" "╮" "│" "╯" "─" "╰" "│" ];
     };
 
