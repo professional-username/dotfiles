278e4449bd83b270b659678826d461b259e22c0e
professional-username <9hacahn5@anonaddy.me>
2024-06-29 14:30:02 +0100

    Add vscode settings

--- home/vscode/default.nix
+++ home/vscode/default.nix
@@ -2,5 +2,11 @@
   # You can import other home-manager modules here
   imports = [ ];
 
-  programs.vscode = { enable = true; };
+  stylix.targets.vscode.enable = true;
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [ vscodevim.vim ];
+    # Make sure caps lock means escape as assigned by window manager
+    userSettings = { keyboard.dispatch = "keyCode"; };
+  };
 }
