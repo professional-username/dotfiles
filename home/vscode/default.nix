125a2f66463e590bf3dd0386b94b758c08f54075
professional-username <9hacahn5@anonaddy.me>
2024-06-29 15:08:01 +0100

    Add prettier to vscode

--- home/vscode/default.nix
+++ home/vscode/default.nix
@@ -5,7 +5,10 @@
   stylix.targets.vscode.enable = true;
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [ vscodevim.vim ];
+    extensions = with pkgs.vscode-extensions; [
+      vscodevim.vim
+      esbenp.prettier-vscode
+    ];
     # Make sure caps lock means escape as assigned by window manager
     userSettings = { keyboard.dispatch = "keyCode"; };
   };
