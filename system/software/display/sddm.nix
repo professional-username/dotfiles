f083b67f9960543f5dd8c047387316894cb6c40f
professional-username <9hacahn5@anonaddy.me>
2024-07-31 01:29:06 +0100

    Fix font in sddm

--- system/software/display/sddm.nix
+++ system/software/display/sddm.nix
@@ -1,16 +1,11 @@
 { config, pkgs, lib, defaultWallpaper, ... }:
 
 {
-  # Use sddm for login management
-  services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.sddm.wayland.enable = true;
-  services.displayManager.sddm.enableHidpi = true;
-  # services.xserver.desktopManager.plasma6.enable = true;
-
   # Set the sddm theme
   services.displayManager.sddm = {
-    # enable = true; # Enable SDDM.
+    enable = true; # Enable SDDM.
+    wayland.enable = true;
+    enableHidpi = true;
     sugarCandyNix = {
       enable = true;
       settings = {
@@ -18,8 +13,9 @@
         Background = lib.cleanSource defaultWallpaper;
         ScreenWidth = 3840;
         ScreenHeight = 2160;
-        Font = "Fira Code Bold";
+        Font = "FiraCode Nerd Font";
         # Form settings
+        HeaderText = "Welcome!";
         FormPosition = "left";
         HaveFormBackground = true;
         PartialBlur = true;
