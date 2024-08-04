8fd35b3b8fa79d780f1fc9433ea1a8d587ba4153
professional-username <9hacahn5@anonaddy.me>
2024-08-04 15:42:37 +0100

    Label emacs packages with their purpose

--- home/emacs/packages.nix
+++ home/emacs/packages.nix
@@ -3,24 +3,24 @@
   programs.emacs.extraPackages = epkgs:
     with epkgs; [
       # Essential packages
-      evil
-      vertico
-      orderless
-      consult
-      embark
-      marginalia
-      magit
-      eglot
-      projectile
-      flycheck
-      company
-      treemacs
-      doom-themes
-      general
+      evil # Vim emulation layer
+      vertico # Vertical completion UI
+      orderless # Flexible completion style
+      consult # Search and navigation commands
+      embark # Context-aware action menu
+      marginalia # Annotations for minibuffer completions
+      magit # Git interface
+      eglot # LSP client
+      projectile # Project management
+      flycheck # On-the-fly syntax checking
+      company # Text completion framework
+      treemacs # Tree-style file explorer sidebar
+      doom-themes # Theme collection
+      general # Key binding management
 
       # Additional useful packages
-      which-key
-      helpful
-      rainbow-delimiters
+      which-key # Displays available keybindings
+      helpful # Improved help system
+      rainbow-delimiters # Colorizes nested parentheses
     ];
 }
