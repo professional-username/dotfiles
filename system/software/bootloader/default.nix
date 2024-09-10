b60f0df5bd9aaa4d8b21cc464da2a83ecd92b373
professional-username <9hacahn5@anonaddy.me>
2024-09-10 19:29:45 +0100

    Add support for emulating aarch64-linux systems

--- system/software/bootloader/default.nix
+++ system/software/bootloader/default.nix
@@ -7,6 +7,9 @@
   # boot.loader.grub.device = "/dev/nvme0n1p1";
   # boot.loader.efi.canTouchEfiVariables = true;
 
+  # Enable emulation of other systems
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
   boot.loader.efi.canTouchEfiVariables = true;
 
   boot.loader.grub = {
