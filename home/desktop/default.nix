eed3eeb067ebc38b811eb2f8ff9091f3aebec87e
professional-username <9hacahn5@anonaddy.me>
2024-10-20 16:49:10 +0100

    Add dunst notification daemon

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -1,8 +1,19 @@
-{ inputs, ... }: {
+{ inputs, pkgs, ... }: {
   imports = [
     # Manage Desktop-related configurations
     ./hyprland.nix
     ./swww.nix
     ./ags
   ];
+
+  # Notification daemon
+  services.dunst = {
+    enable = true;
+    settings.global = {
+      offset = "30x30";
+      corner_radius = "12";
+      padding = "12";
+      horizontal_padding = "12";
+    };
+  };
 }
